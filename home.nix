89fe86213e3418be9dd4d60e0a72bb6ed90ed8f6
Asherah Connor <ashe@kivikakk.ee>
2024-07-19 12:24:39 +0300

    home: clean up packages a lil.

--- home.nix
+++ home.nix
@@ -19,30 +19,33 @@
       EDITOR = "hx";
     };
 
-    packages = with pkgs; [
-      vyxos-git
-      age
-      wget
-      nix-tree
-      # binutils # Do Not. This messes up things on macOS.
-      unzip
-      inetutils
-      htop
-      clang-tools_16 # for clang-format
-      file
-      dig
-      lsof
-      fx # JSON viewer
-      jq
-      docutils # python
-      bundix # ruby
-      tio
-      fd
-      cowsay
-      ssh-to-age
-      sops
-      unixtools.xxd
-      httpie
-    ];
+    packages = with pkgs; ([
+        # Tools provided by or useful with the config.
+        vyxos-git
+        cowsay # :|3
+        age
+        sops
+        ssh-to-age
+        nix-tree
+      ]
+      ++ [
+        # Generally helpful.
+        dig
+        file
+        htop
+        httpie
+        inetutils
+        jq
+        lsof
+        tio
+        unixtools.xxd
+        unzip
+        wget
+        # binutils # Do Not. This messes up things on macOS.
+      ] ++ [
+        # Things I keep meaning to learn to use properly.
+        fd
+        fx
+      ]);
   };
 }
