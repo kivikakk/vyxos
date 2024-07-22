0d3064d973c8c6a899e375bf362b99877a2737f9
Asherah Connor <ashe@kivikakk.ee>
2024-07-22 20:26:20 +0300

    seraphim: add zls to path for helix.

--- hosts/seraphim.nix
+++ hosts/seraphim.nix
@@ -52,6 +52,7 @@
           fish_add_path $HOME/.local/bin
           fish_add_path $HOME/.ghcup/bin
           fish_add_path $HOME/.cabal/bin
+          fish_add_path $HOME/g/zls/zig-out/bin
           alias surfer $HOME/g/surfer/target/release/surfer
         '';
       };
