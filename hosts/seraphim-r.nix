e694837e3db0a9f4d83df5881701a998b1635214
Asherah Connor <ashe@kivikakk.ee>
2024-08-01 14:20:02 +0300

    seraphim-r: populate .aws/credentials.

--- hosts/seraphim-r.nix
+++ hosts/seraphim-r.nix
@@ -8,4 +8,17 @@
     brews = ["awscli"];
     casks = ["mattermost"];
   };
+
+  vyxos.secrets.encrypted = {
+    "aws-credentials" = {};
+  };
+
+  home-manager.users.${config.vyxos.vyxUser} = let
+    inherit (config) vyxos;
+  in
+    {config, ...}: {
+      # XXX: This may or may not correctly instantiate correctly without first
+      # building with just the secret above. If so -- sorry!
+      home.file.".aws/credentials".source = config.lib.file.mkOutOfStoreSymlink vyxos.secrets.decrypted."aws-credentials".path;
+    };
 }
