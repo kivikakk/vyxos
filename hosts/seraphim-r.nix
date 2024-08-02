8b17cea2609388f842aa33b5b950de7e0e62b63e
Asherah Connor <ashe@kivikakk.ee>
2024-08-02 10:09:24 +0300

    seraphim-r: install fzf, symlink ed25519.

--- hosts/seraphim-r.nix
+++ hosts/seraphim-r.nix
@@ -2,6 +2,8 @@
   config,
   pkgs,
   lib,
+  vyxUser,
+  hostName,
   ...
 }: {
   homebrew = {
@@ -17,6 +19,9 @@
     inherit (config) vyxos;
   in
     {config, ...}: {
+      home.packages = [pkgs.fzf];
+      # XXX: Hecc this
+      home.file.".ssh/radiopaedia.pem".source = config.lib.file.mkOutOfStoreSymlink "/Users/kivikakk/.ssh/id_ed25519";
       # XXX: This may or may not correctly instantiate correctly without first
       # building with just the secret above. If so -- sorry!
       home.file.".aws/credentials".source = config.lib.file.mkOutOfStoreSymlink vyxos.secrets.decrypted."aws-credentials".path;
