01a82186b0884afafa273f6faa616fa116c702df
Asherah Connor <ashe@kivikakk.ee>
2024-08-03 15:22:53 +0300

    aerc: no longer depending on this.

--- home/aerc.nix
+++ home/aerc.nix
@@ -23,10 +23,6 @@
 
   programs.aerc = {
     enable = true;
-    # Starting with the default binds, since home-manager 23.05's aerc module
-    # improperly always creates binds.conf, even if nothing is configured,
-    # thus unsetting all binds.
-    extraBinds = builtins.readFile ./aerc-binds.conf;
 
     extraConfig = {
       # https://github.com/nix-community/home-manager/blob/0f4e5b4999fd6a42ece5da8a3a2439a50e48e486/modules/programs/aerc.nix#L167-L177
