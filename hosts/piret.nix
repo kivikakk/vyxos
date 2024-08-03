b842d851964afc460f90f4925588ff4b43b2fc4c
Asherah Connor <ashe@kivikakk.ee>
2024-08-03 14:50:48 +0300

    piret: install iosevka.

--- hosts/piret.nix
+++ hosts/piret.nix
@@ -151,6 +151,7 @@
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     environment.systemPackages = with pkgs; [
+      iosevka
       #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #  wget
     ];
