c5439b5f2325cd53840ea8c2cf0e624578caa469
Asherah Connor <ashe@kivikakk.ee>
2024-08-03 15:33:52 +0300

    piret: clean up config a little.

--- hosts/piret.nix
+++ hosts/piret.nix
@@ -13,24 +13,26 @@
     boot.kernelModules = ["kvm-amd"];
     boot.extraModulePackages = [];
 
-    # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
-    fileSystems."/" = {
-      device = "/dev/disk/by-uuid/9ca66df7-2812-470b-9ac7-69594af39e38";
-      fsType = "btrfs";
-      options = ["subvol=@"];
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/9ca66df7-2812-470b-9ac7-69594af39e38";
+        fsType = "btrfs";
+        options = ["subvol=@"];
+      };
+      "/boot" = {
+        device = "/dev/disk/by-uuid/12CE-A600";
+        fsType = "vfat";
+        options = ["fmask=0022" "dmask=0022"];
+      };
     };
 
-    boot.initrd.luks.devices."luks-9199b301-3f97-4c18-8149-9ff01094c792".device = "/dev/disk/by-uuid/9199b301-3f97-4c18-8149-9ff01094c792";
-
-    fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/12CE-A600";
-      fsType = "vfat";
-      options = ["fmask=0022" "dmask=0022"];
+    boot.initrd.luks.devices = {
+      "luks-9199b301-3f97-4c18-8149-9ff01094c792".device = "/dev/disk/by-uuid/9199b301-3f97-4c18-8149-9ff01094c792";
+      "luks-e7d3a4b7-7ac7-466e-b402-dbe5b34a616c".device = "/dev/disk/by-uuid/e7d3a4b7-7ac7-466e-b402-dbe5b34a616c";
     };
-    boot.initrd.luks.devices."luks-e7d3a4b7-7ac7-466e-b402-dbe5b34a616c".device = "/dev/disk/by-uuid/e7d3a4b7-7ac7-466e-b402-dbe5b34a616c";
 
     swapDevices = [
       {device = "/dev/disk/by-uuid/0463f7f5-d50b-4a87-afc0-9c0b737e801d";}
@@ -43,43 +45,15 @@
     lib,
     ...
   }: {
-    vyxos.secrets = {
-      additionalAuthorizedKeys = [
-        "kivikakk-celica"
-        "kivikakk-estrellita"
-      ];
-      encrypted = {
-        "aerc-password" = {};
-      };
-    };
-
-    services.tailscale.enable = true;
-
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    networking.useDHCP = lib.mkDefault true;
-    # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
-
+    system.stateVersion = "24.05";
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Enable networking
+    networking.useDHCP = lib.mkDefault true;
     networking.networkmanager.enable = true;
 
-    # Set your time zone.
-    time.timeZone = "Europe/Tallinn";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-    # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
-
     i18n.extraLocaleSettings = {
       LC_ADDRESS = "et_EE.UTF-8";
       LC_IDENTIFICATION = "et_EE.UTF-8";
@@ -92,28 +66,21 @@
       LC_TIME = "et_EE.UTF-8";
     };
 
-    # Enable the X11 windowing system.
     services.xserver.enable = true;
 
-    # Enable the GNOME Desktop Environment.
     # services.xserver.displayManager.gdm.enable = true;
     # services.xserver.desktopManager.gnome.enable = true;
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
 
-    # Configure keymap in X11
     services.xserver = {
       layout = "us";
       xkbVariant = "dvorak";
     };
-
-    # Configure console keymap
     console.keyMap = "dvorak";
 
-    # Enable CUPS to print documents.
     services.printing.enable = true;
 
-    # Enable sound with pipewire.
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
@@ -121,24 +88,9 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
     };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.kivikakk = {
-      extraGroups = ["networkmanager"];
-    };
-
-    # Install firefox.
-    programs.firefox.enable = true;
+    users.users.kivikakk.extraGroups = ["networkmanager"];
 
     nixpkgs.config.allowUnfreePredicate = pkg:
       builtins.elem (lib.getName pkg) [
@@ -147,49 +99,37 @@
         "1password"
       ];
 
-    programs._1password.enable = true;
-    programs._1password-gui = {
-      enable = true;
-      polkitPolicyOwners = ["kivikakk"];
+    programs = {
+      firefox.enable = true;
+
+      _1password.enable = true;
+      _1password-gui = {
+        enable = true;
+        polkitPolicyOwners = ["kivikakk"];
+      };
     };
 
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
     environment.systemPackages = with pkgs; [
       iosevka
-      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      #  wget
     ];
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    networking.firewall.allowedTCPPorts = [22];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.05"; # Did you read the comment?
+    vyxos = {
+      desktop.enable = true;
+      secrets = {
+        additionalAuthorizedKeys = [
+          "kivikakk-celica"
+          "kivikakk-estrellita"
+        ];
+        encrypted = {
+          "aerc-password" = {};
+        };
+      };
+    };
 
     home-manager.users.${config.vyxos.vyxUser} = {
       accounts.email.accounts.asherah.passwordCommand = "cat ${config.vyxos.secrets.decrypted."aerc-password".path}";
     };
+
+    services.tailscale.enable = true;
   };
 }
