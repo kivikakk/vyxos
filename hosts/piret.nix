4c4e2b1bc245f271c241d95d86d56e39164b262d
Asherah Connor <ashe@kivikakk.ee>
2024-08-03 13:56:50 +0300

    piret: try plasma6.

--- hosts/piret.nix
+++ hosts/piret.nix
@@ -91,8 +91,10 @@
     services.xserver.enable = true;
 
     # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    # services.xserver.displayManager.gdm.enable = true;
+    # services.xserver.desktopManager.gnome.enable = true;
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
 
     # Configure keymap in X11
     services.xserver = {
