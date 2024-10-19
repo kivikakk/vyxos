9a9fa04d080d475d49b4a6823f016aad81d8b764
Asherah Connor <ashe@kivikakk.ee>
2024-10-19 18:39:28 +1100

    jujutsu: default name.

--- home/jujutsu.nix
+++ home/jujutsu.nix
@@ -2,6 +2,10 @@
   programs.jujutsu = {
     enable = true;
     settings = {
+      user = {
+        name = "Asherah Connor";
+        email = "ashe@kivikakk.ee";
+      };
       ui = {
         default-command = ["status"];
         paginate = "never";
