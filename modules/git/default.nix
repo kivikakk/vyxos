01c333d60d4d08b02821b7aa2528146747fce7d7
Charlotte <charlotte@lottia.net>
2024-10-21 18:59:09 +1100

    git: try doInstallCheck = false

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -3,6 +3,7 @@
     nixpkgs.config.packageOverrides = oldPkgs: {
       vyxos-git = vyxos-git-base.overrideAttrs {
         doCheck = false;
+        doInstallCheck = false;
         patches = [
           ./pretty.c.patch
         ];
