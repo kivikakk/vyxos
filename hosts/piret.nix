f8303c690b00b75f8e607cb5e1e988ab2100ca1c
Charlotte <charlotte@lottia.net>
2024-11-26 18:50:11 +1100

    piret: add aarch64-linux binfmt emulation

--- hosts/piret.nix
+++ hosts/piret.nix
@@ -15,6 +15,7 @@
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
     fileSystems = {
       "/" = {
