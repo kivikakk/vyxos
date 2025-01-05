4be2c693e368ebdaf9fc0b5741ee02e7ed1262d1
Charlotte <charlotte@lottia.net>
2025-01-05 12:02:17 +1100

    desktop: split iosevka into its own file

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -12,6 +12,7 @@ in {
     ./gui.nix
     ./mpd.nix
     ./firefox.nix
+    ./iosevka.nix
   ];
 
   options.vyxos.desktop = {
@@ -50,32 +51,6 @@ in {
         polkitPolicyOwners = [vyxUser];
       };
     };
-
-    fonts.packages = with pkgs; [
-      (iosevka.override {
-        set = "TermSlab";
-        privateBuildPlan = {
-          family = "Iosevka Term Slab";
-          spacing = "term";
-          serifs = "slab";
-          noCvSs = false;
-          exportGlyphNames = true;
-
-          widths = {
-            Condensed = {
-              shape = 500;
-              menu = 3;
-              css = "condensed";
-            };
-            Normal = {
-              shape = 600;
-              menu = 5;
-              css = "normal";
-            };
-          };
-        };
-      })
-    ];
 
     home-manager.users.${vyxUser} = {
       home.packages = with pkgs; [
